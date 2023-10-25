568ddb467e7ff4ccf5236329c91ae7c6c7ed70cc
Ahmed Darwish <darwish@visions-tech.net>
2023-10-25 07:20:40 +0300

    first commit

---
+++ Controllers/Core/RMCharacterVC.swift
@@ -0,0 +1,29 @@
+//
+//  RMCharacterVC.swift
+//  RickAndMorty
+//
+//  Created by Ahmed Darwish on 23/10/2023.
+//
+
+import UIKit
+
+class RMCharacterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
